73513011cb0a0879f875855794ed30c4f367dcc3
JuleeCachau <juliancachau@gmail.com>
2026-02-01 03:35:44 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // auth.js — Registro/Login usando Google Apps Script (Google Sheets)
 // Configurá tu URL del Web App acá:
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwa06wBhwA2KuV4psLdx1ZpCz9FlcUOa3CBpQp_FlxBhGgSfdl0hdDGJWgUefF-p0aITA/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyzYn6JN6iQLGvI8hP4FdL2mQOdqJVpAc9u71b1C_cQu-nZlc56EE0Xw1ZHlI3ABi6tCw/exec";
 
 // Reglas de password:
 // - mínimo 8
