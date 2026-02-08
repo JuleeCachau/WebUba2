6b091c3efbd10d708baa5e5c63421eccd8a733eb
JuleeCachau <juliancachau@gmail.com>
2026-02-08 19:21:22 -0300

    Update APPS_SCRIPT_URL to new endpoint

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 // auth.js — Registro/Login + datos usando Google Apps Script (Google Sheets)
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxzBKbLSFuSRvcD5tnSGVW863qWqXu1ua5Cbu4a6qj8fQiUHvpH1A7Dc7txkbEhasgb/exec"; // <-- si cambia tu WebApp (/exec), reemplazá esta URL
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxPeWTfUGeQrFbPWB9vYzpBl2EMaPpT7kEIJcQgCFOIfusoI4NOSO0GW8V_sLIQ8dKc/exec"; // <-- si cambia tu WebApp (/exec), reemplazá esta URL
 
 // ===== Validaciones =====
 export function validateDni(dni) {
